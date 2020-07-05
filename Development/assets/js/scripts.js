c30975fe5aa5d6c0f98919047678de49795cafe3
fatmahsaleemah <66628606+fatmahsaleemah@users.noreply.github.com>
2020-07-05 23:03:20 +0300

    تم الحل

--- Development/assets/js/scripts.js
+++ Development/assets/js/scripts.js
@@ -4,8 +4,9 @@ let arr = [];
 console.log(arr);
 
 // onClick هذه الدالة المسؤولة عن إضافة العنصر في السلة عن طريق
-function addToCart() {
+function addToCart(item) {
   // أكمل
+  arr.push(item);
 }
 
 let ul = document.createElement("ul");
@@ -15,9 +16,16 @@ document.getElementById("container").appendChild(ul);
 // create(arr) هذه الدالة المسؤولة عن استدعاء
 function myFunction() {
   // أكمل
+  create(arr);
 }
 
 // arr هذه الدالة المسؤولة عن إنشاء قائمة المشتريات من العناصر الموجودة في المصفوفة
 function create(arr) {
   // أكمل
+  arr.forEach(function (data) {
+    let li = document.createElement("li");
+    ul.appendChild(li);
+    li.innerHTML += data;
+    li.className = "myList";
+  });
 }
